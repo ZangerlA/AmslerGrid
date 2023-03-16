b6fec5853196c4b64e8e793959b2e93a8614ae58
MaximilianGrossmann <69078512+MaximilianGrossmann@users.noreply.github.com>
2023-03-16 20:18:50 +0100

    Add Grid/CenterPoint/Mouseeventlistener(without use)

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Canvas from "./Canvas";
 
@@ -9,7 +8,7 @@ function App() {
 	
 	return (
 		<div className="App">
-			<Canvas/>
+			<Canvas />
 		</div>
 	);
 }
--- src/Canvas.tsx
+++ src/Canvas.tsx
@@ -1,28 +1,104 @@
-import {FC, useEffect, useRef, useState} from "react";
+import {FC, useEffect, useRef, useState, MouseEvent} from "react";
+import useWindowDimensions from "./UseWindowDimensions";
+
+type Coordinates = {
+	x: number,
+	y: number,
+}
 
 const Canvas: FC = (props) => {
 	
 	const canvasRef = useRef<HTMLCanvasElement>(null)
+	const { width, height } = useWindowDimensions()
 	const [ctx, setCtx] = useState<CanvasRenderingContext2D>()
-	
-	const draw = (ctx: CanvasRenderingContext2D) => {
-		ctx.fillStyle = "green";
-		ctx.fillRect(10, 10, 1500, 1000);
+	const [ mouseClick, setMouseClick ] = useState<Coordinates>({x:0, y:0})
+	const [ grid, setGrid] = useState<Coordinates[][]>([])
+
+	const calculateCoordinates = () => {
+		let grid: Coordinates[][] = []
+		const cellSizeHorizontal = (width-100) / 5
+		const cellSizeVertical = (height-100) / 5
+		for (let i = 0; i < 6; i++) {
+			grid[i] = []
+			for (let j = 0; j < 6; j++) {
+				grid[i][j] = {x: (cellSizeHorizontal*i)+50, y: (cellSizeVertical*j)+50}
+			}
+		}
+		setGrid(grid)
 	}
-	
+
+	const draw = (ctx: CanvasRenderingContext2D) => {
+		ctx.clearRect(0, 0, width, height)
+		drawGridLines(ctx)
+		drawHelpPoints(ctx)
+		drawCenterPoint(ctx)
+	}
+
+	const drawCenterPoint = (ctx: CanvasRenderingContext2D) => {
+		const center: Coordinates = {x: width/2, y: height/2}
+
+		ctx.beginPath()
+		ctx.fillStyle = "red";
+		//ctx.fillRect(center.x,center.y, 10,10)
+		ctx.arc(center.x, center.y, 10, 0, Math.PI*4, false)
+		ctx.fill()
+
+	}
+
+	const drawGridLines = (ctx: CanvasRenderingContext2D) => {
+		ctx.beginPath()
+		for (let i = 0; i < grid.length; i++) {
+			ctx.moveTo(grid[i][0].x, grid[i][0].y)
+			for (let j = 0; j < grid[i].length; j++) {
+				const point = grid[i][j]
+				ctx.lineTo(point.x, point.y)
+				if (i+1 < grid.length){
+					ctx.lineTo(grid[i+1][j].x, grid[i+1][j].y)
+				}
+				ctx.moveTo(point.x, point.y)
+			}
+		}
+		ctx.stroke()
+	}
+
+	const drawHelpPoints = (ctx: CanvasRenderingContext2D) => {
+		ctx.beginPath()
+		for (let i = 0; i < grid.length; i++) {
+			for (let j = 0; j < grid[i].length; j++) {
+				const coordinate = grid[i][j]
+				ctx.moveTo(grid[i][j].x, grid[i][j].y)
+				ctx.fillStyle = "black";
+				ctx.arc(coordinate.x, coordinate.y, 7, 0, Math.PI*2, false)
+				ctx.fill()
+			}
+		}
+	}
+
+	const handleClick = (event: MouseEvent)=>{
+		event.preventDefault()
+		setMouseClick({x: event.clientX, y: event.clientY})
+	}
+
 	useEffect(() => {
 		const canvas = canvasRef.current
 		setCtx((canvas!.getContext('2d'))!)
-		
-		if (ctx) {
+	}, [])
+
+	useEffect(() => {
+		if(ctx) {
 			draw(ctx)
 		}
-		
-	},[ctx])
+	}, [ctx, grid])
+
+	useEffect(() => {
+		calculateCoordinates()
+	}, [width, height])
 	
 	return (
-		<canvas ref={canvasRef} {...props}></canvas>
+		<canvas onClick={handleClick} ref={canvasRef} width={width} height={height} {...props}></canvas>
 	)
 }
+
+// style={{boxShadow: "inset 0px 0px 0px 3px green"}}
 
 export default Canvas
